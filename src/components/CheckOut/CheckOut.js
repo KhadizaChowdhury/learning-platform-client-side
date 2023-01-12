879959ee24b9e01799680a3845d57bfed4dfff84
KhadizaChowdhury <khadizachowdhury70@gmail.com>
2023-01-12 08:48:22 +0600

    checkout page added

--- src/components/CheckOut/CheckOut.js
+++ src/components/CheckOut/CheckOut.js
@@ -4,7 +4,7 @@ import { useLoaderData } from 'react-router-dom';
 const CheckOut = () => {
     const getAccess = useLoaderData()
     // console.log(getAccess[0])
-    const { _id, title, image_url, category_id, details } = getAccess[0];
+    const { _id, title, image_url, category_id, details } = getAccess;
     return (
         <div>
             Course Name: {title}
